edacd613c85e59b01c9949181a5e82377950bc86
Hailey Prevett <haileyp@qualtrics.com>
2020-06-25 20:07:33 -0400

    maybe pass up to #12 of mod 1

--- stats/data.py
+++ stats/data.py
@@ -1,15 +1,31 @@
+# Import built in libraries
 import os
 import glob
+
+# Import Pandas
 import pandas as pd
 
+# Python file management
 game_files = glob.glob(os.path.join(os.getcwd(),'games','*.EVE'))
 game_files.sort()
 
+# Sorting file names
 game_frames = []
 
+# Read CSV files & append game frames
 for game_file in game_files:
     game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3','multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
 
+# Concanenate game frames
 games = pd.concat(game_frames)
+
+# Clean values
 games.loc[games['multi5'] == '??', ['multi5']] = ''
+
+# Extract & forward fill identifiers
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+identifiers = identifiers.fillna(method='ffill')
+
+# Rename columns
+identifiers.columns = ['game_id', 'year']
