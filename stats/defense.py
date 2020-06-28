da864132a71400a7e3392ec306569a03bb2953e6
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 18:48:04 -0400

    mod 6.2

--- stats/defense.py
+++ stats/defense.py
@@ -4,4 +4,12 @@ from frames import games
 from frames import info
 from frames import events
 
-# plays = games.query('type'=='play' & 'event'!='NP')
+# Keep games where type is play and event is not NP
+plays = games.query("type=='play' & event!='NP'")
+# Rename columns
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+
+# Remove rows for same consecutive at bat
+pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
+# Group plate appearances
+pa = pa.groupby(['year','game_id','team']).size().reset_index(name='PA')
