331aad59a78ed18f5931d773d62ea0dede87bba4
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 17:24:42 -0400

    mod 2.2

--- stats/attendance.py
+++ stats/attendance.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 import games from data.py
 
 # Select appropriate columns from games DataFrame
-attendance = [[ games.loc[games['type'] == 'info',
-                games['multi2'] == 'attendance' ],
-              [ 'year', 'multi3' ] ]
+# = [[ games.loc[games['type'] == 'info',
+#                games['multi2'] == 'attendance' ],
+#              [ 'year', 'multi3' ] ]
