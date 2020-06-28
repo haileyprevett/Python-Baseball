7ec94991116a6a562916f6e12209b14a808ca72b
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 18:33:20 -0400

    mod 4.8

--- stats/offense.py
+++ stats/offense.py
@@ -28,7 +28,7 @@ hit_type = hits['event'].replace(replacements,  regex=True)
 hits = hits.assign(hit_type=hit_type)
 
 # Group by inning and hit type
-hits = hits.groupby([inning,hit_type]).size().reset_index(name='count')
+hits = hits.groupby(['inning','hit_type']).size().reset_index(name='count')
 
 # Convert hit type col to categorical
 hits.loc[:,'hit_type'] = pd.Categorical(hits.loc[:,'hit_type'],['single', 'double', 'triple', and 'hr'])
