4ae8cbe311f946180776ad47d777cbc02f683a45
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 17:59:52 -0400

    mod 3.4

--- stats/pitching.py
+++ stats/pitching.py
@@ -11,3 +11,10 @@ strike_outs = plays[plays['event'].str.contains('K')]
 
 # Group by year and game
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+# Reset index
+strike_outs = strike_outs.reset_index(name='strike_outs')
+# Convert number of strike outs to numeric
+strike_outs = strike_outs.loc[:,['year', 'strike_outs']].apply(pd.to_numeric)
+
+strike_outs.plot(x='year', y='strike_outs', kind='scatter').legend('Strike outs')
+plt.show()
