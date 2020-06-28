b58f2e704b892336a6c55d66a1c5f6372e95c4ff
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 18:41:01 -0400

    check import statements mod 6

--- stats/defense.py
+++ stats/defense.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from frames import games
+from frames import info
+from frames import events
+
+# plays = games.query('type'=='play' & 'event'!='NP')
