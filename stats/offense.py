2cca9ce8c0a0dcb95c2036208ea9e650a9f6a1f5
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 18:12:49 -0400

    mod 4.2

--- stats/offense.py
+++ stats/offense.py
@@ -7,10 +7,7 @@ plays = games[games['type'] == 'play']
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 
 # Select the rows where the event column's value starts with S (not SB), D, T, and HR
-plays = plays[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)')]
 # Keep only inning and event cols and assign to new var hits
-hits = plays[:,['inning', 'event']]
-
-# strike_outs = strike_outs.loc[:,['year', 'strike_outs']].apply(pd.to_numeric)
+hits = plays[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),['inning', 'event']]
 
 hits = hits.loc[:,['inning']].apply(pd.to_numeric)
