b9630b88ec157f291ed38130c2b43d41888889c2
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 17:35:58 -0400

    mod 2.6

--- stats/attendance.py
+++ stats/attendance.py
@@ -10,5 +10,10 @@ attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendan
 # Change col names in DataFrame
 attendance.columns = ['year','attendance']
 
-# Convert attendance col to numeric 
+# Convert attendance col to numeric
 attendance.loc[:,'attendance'] = pd.to_numeric(attendance.loc[:,'attendance'])
+
+plt.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+plt.show()
