73d072a9b5f2fde1e55ecd7c6ee94a1603216195
Hailey Prevett <haileyp@qualtrics.com>
2020-06-28 17:50:12 -0400

    mod 3.3

--- stats/pitching.py
+++ stats/pitching.py
@@ -4,11 +4,10 @@ import matplotlib.pyplot as plt
 from data import games
 
 # Grab games of type "play"
-plays = games.loc[games['type'] == 'play']
+plays = games[games['type'] == 'play']
 
 # Select all strike outs
 strike_outs = plays[plays['event'].str.contains('K')]
 
 # Group by year and game
-strike_outs.groupby(['year', 'game_id'])
-# strike_outs = strike_outs.size()
+strike_outs = strike_outs.groupby(['year', 'game_id']).size()
